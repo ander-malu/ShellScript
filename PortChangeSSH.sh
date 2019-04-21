a239395a527d2cb342d7efd43fed18d302c58df6
ander-malu <anderson.almeidapy@gmail.com>
2019-04-21 05:23:25 -0300

    Add the first script

---
+++ PortChangeSSH.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+echo "=== Change SSH port to 2259 ==="
+echo " "
+sleep 3
+
+sed -i 's/#Port [0-9]\+$/Port 2259/' /etc/ssh/sshd_config
+
+/etc/init.d/ssh restart && sleep 1 && /etc/init.d/ssh status
+
+exit
